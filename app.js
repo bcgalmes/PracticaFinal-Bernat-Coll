04567b9fb574e78a4b13a15fa6f56fd2c0be68a2
bcgalmes <94987564+bcgalmes@users.noreply.github.com>
2022-02-23 22:27:50 +0100

    commit

---
+++ app.js
@@ -0,0 +1,89 @@
+const formulario = document.getElementById('formulario');
+const inputs = document.querySelectorAll('#formulario input');
+
+
+const expresiones = {
+	nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, 
+    apellidos: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, //Solo permite letras para el apellido
+	password: /^(?=(?:.*\d){1})(?=(?:.*[A-Z]){1})(?=(?:.*[a-z]){1})(?=(?:.*[@$?¡\-_]){1})\S{8,16}$/, //Obliga a tener alemnos una mayuscula, un numero, y un caracter especial.
+	correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,	//Obliga a poner el "@" y .algo"								  
+}
+
+const campos = {
+	nombre: false,
+	apellidos: false,
+	correo: false,
+	password: false
+
+}
+
+const validarFormulario = (e) =>{
+
+	switch(e.target.name){
+		case "nombre":
+		validarCampo(expresiones.nombre, e.target, 'nombre');
+	
+		break;
+		case "apellidos":
+			validarCampo(expresiones.apellidos, e.target, 'apellidos');
+		break;
+		case "correo":
+			validarCampo(expresiones.correo, e.target, 'correo');
+		break;
+		case "password":
+			validarCampo(expresiones.password, e.target, 'password');
+						validarPassword2();
+		case "password2":
+			validarPassword2();
+		break;
+	} 
+
+}
+
+
+const validarCampo = (expresion, input, campo) => {
+	if(expresion.test(input.value)){
+		document.getElementById(`grupo__${campo}`).classList.remove('formulario__grupo-incorrecto');
+		document.getElementById(`grupo__${campo}`).classList.add('formulario__grupo-correcto');
+		document.querySelector(`#grupo__${campo} .formulario__input-error`).classList.remove('formulario__input-error-activo');
+		campos[campo] = true;
+	}else{
+		document.getElementById(`grupo__${campo}`).classList.add('formulario__grupo-incorrecto');
+		document.getElementById(`grupo__${campo}`).classList.remove('formulario__grupo-correcto');
+		document.querySelector(`#grupo__${campo} .formulario__input-error`).classList.add('formulario__input-error-activo');
+		campos[campo] = false;
+	}	
+}
+
+
+const validarPassword2 = () => {
+	const inputPassword1 = document.getElementById('password');
+	const inputPassword2 = document.getElementById('password2');
+
+	if(inputPassword1.value !== inputPassword2.value){
+		document.getElementById(`grupo__password2`).classList.add('formulario__grupo-incorrecto');
+		document.getElementById(`grupo__password2`).classList.remove('formulario__grupo-correcto');
+		document.querySelector(`#grupo__password2 .formulario__input-error`).classList.add('formulario__input-error-activo');
+		campos['password'] = false;
+	} else {
+		document.getElementById(`grupo__password2`).classList.remove('formulario__grupo-incorrecto');
+		document.getElementById(`grupo__password2`).classList.add('formulario__grupo-correcto');
+		document.querySelector(`#grupo__password2 .formulario__input-error`).classList.remove('formulario__input-error-activo');
+		campos['password'] = true;
+	}
+}
+
+inputs.forEach((input) => {
+	input.addEventListener('keyup', validarFormulario);
+	input.addEventListener('blur', validarFormulario);
+});
+
+
+formulario.addEventListener('submit', (e) => {
+e.preventDefault();
+if(campos.nombre && campos.apellidos && campos.correo && campos.password ){
+formulario.reset();
+
+}
+
+});
